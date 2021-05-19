1a8be83d88e62f461e7df9b66712cc93da01ec50
Elsa Passioni <elsa.passioni@gmail.com>
2021-05-20 00:02:35 +0200

    Publish gh-pages

--- scripts/lib/is-path.js
+++
@@ -1,2 +0,0 @@
-const PATH_PATTERN = /(\.{1,2})?(\w+)?\//;
-module.exports = str => PATH_PATTERN.test(str);
--- src/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux';
-
-const noop = () => ({})
-
-export default combineReducers({ noop });
